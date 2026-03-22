eaf58a328e6cc2035a699a1526ff31667c4ce182
Anirban Mondal <112965065+Anirban3030@users.noreply.github.com>
2026-03-23 01:21:35 +0530

    Refactor EmployeeController

--- 20.03.26 Spring Boot Assignment – EMS One-to-One Mapping/emsOnetoOnemap/emsOnetoOnemap/src/main/java/com/capgemini/emsOnetoOnemap/controller/EmployeeController.java
+++ 20.03.26 Spring Boot Assignment – EMS One-to-One Mapping/emsOnetoOnemap/emsOnetoOnemap/src/main/java/com/capgemini/emsOnetoOnemap/controller/EmployeeController.java
@@ -18,31 +18,28 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    // POST → Create Employee with Address
+
     @PostMapping("/save")
     public String saveEmployee(@RequestBody @Valid EmployeeDTO employeeDTO) {
         return employeeService.saveEmployee(employeeDTO);
     }
 
-    // GET → Get all Employees
     @GetMapping("/find-all")
     public List<Employee> getAllEmployees() {
         return employeeService.getallEmployeeswAddress();
     }
 
-    // GET → Get Employee by ID
+
     @GetMapping("/find-by-id/{id}")
     public Employee getEmployeeById(@PathVariable("id") int id) {
         return employeeService.getEmployeebyId(id);
     }
 
-    // PUT → Update Employee and Address
     @PutMapping("/update/{id}")
     public EmployeeDTO updateEmployee(@PathVariable("id") int id, @RequestBody EmployeeDTO employeeDTO) {
         return employeeService.updateEmployee(id, employeeDTO);
     }
 
-    // DELETE → Delete Employee
     @DeleteMapping("/delete/{id}")
     public String deleteEmployee(@PathVariable("id") int id) {
         return employeeService.deleteEmployee(id);
